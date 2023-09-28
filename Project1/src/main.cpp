de5952f2a0f6648a85a50765d9b2c173951b2477
Stephen Gudijanto <stephen.gudijanto@gmail.com>
2023-09-28 23:40:49 +0800

    indexing >> BPlusTree implementation

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Storage/Record.h"
 #include "Storage/Parser.h"
 #include "Storage/Storage.h"
+#include "Indexing/BPlusTree.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -17,5 +18,39 @@ int main() {
     }
     int noOfFullBlocks = storage.getNumOfFullBlocks();
     std::cout << "Done writing record to Storage: " + std::to_string(noOfFullBlocks) << std::endl;
+    BPlusTree tree;
+
+    // Insert key-value pairs into the B+ tree
+    tree.insert(10.5, nullptr);
+    tree.insert(5.0, nullptr);
+    tree.insert(20.0, nullptr);
+    tree.insert(15.0, nullptr);
+    tree.insert(25.0, nullptr);
+    tree.insert(12.0, nullptr);
+    tree.insert(11.0, nullptr);
+    tree.insert(12.0, nullptr);
+    tree.insert(6.0, nullptr);
+    tree.insert(19.0, nullptr);
+    tree.insert(26.0, nullptr);
+    tree.insert(27.0, nullptr);
+    tree.insert(11.5, nullptr);
+    tree.insert(19.5, nullptr);
+    tree.insert(28.0, nullptr);
+
+    tree.getRootValue();
+    std::cout << "Number of Levels: " << tree.getNumberOfLevels() << std::endl;
+    std::cout << "Number of Nodes: " << tree.getNumberOfNodes() << std::endl;
+
+    tree.printTree();
+    float keyToSearch = 19.5;
+    bool found = tree.search(keyToSearch);
+    if (found) {
+        std::cout << "Key " << keyToSearch << " found!" << std::endl;
+        std::cout << "Index nodes accessed: " << tree.getIndexNodeAccessCount() << std::endl;
+        std::cout << "Data blocks accessed: " << tree.getDataBlockAccessCount() << std::endl;
+    } else {
+        std::cout << "Key " << keyToSearch << " not found." << std::endl;
+    }
+
     return 0;
 }
